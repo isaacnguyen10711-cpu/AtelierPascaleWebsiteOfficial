eed32ba76bf5edc41c00d44330864dad80d40758
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-07-02 04:26:00 +0930

    Added handleLogout

--- ap-frontend/src/components/NavBar.jsx
+++ ap-frontend/src/components/NavBar.jsx
@@ -3,7 +3,18 @@ import LoginPage from '../customerPages/LoginPage';
 function NavBar() {
   const location = useLocation();
   const isLoginPage = location.pathname === '/login';
+  let isLoggedIn;
+  if (localStorage.getItem('token')) {
+    isLoggedIn = true;
+  }
+  else {
+    isLoggedIn = false;
+  }
 
+  function handleLogout() {
+    localStorage.removeItem('token');
+    window.location.reload();
+  }
 
   return (
     <nav className="fixed top-0 left-0 z-50 w-full bg-transparent border-b border-white shadow-md">
@@ -11,7 +22,8 @@ function NavBar() {
         <Link to="/">Home</Link>
         <Link to="/products/lacquer">Lacquer</Link>
         <Link to="/products/art">Art</Link>
-        {!isLoginPage && <Link to="/login" className={`flex flex-1 justify-end ${isLoginPage ? 'hover:text-gray-500' : 'hover:text-gray-300'}`}> Login</Link>}
+        {isLoggedIn ? (<button onClick={handleLogout} className={`flex flex-1 justify-end ${isLoginPage ? 'hover:text-gray-500' : 'hover:text-gray-300'}`}>Log out</button>)
+          : (<Link to="/login" className={`flex flex-1 justify-end ${isLoginPage ? 'hover:text-gray-500' : 'hover:text-gray-300'}`}>Log in</Link>)}
       </div>
     </nav>
   )
