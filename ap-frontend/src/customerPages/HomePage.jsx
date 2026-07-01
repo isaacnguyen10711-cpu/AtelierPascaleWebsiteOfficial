22c7115fb0587a42fde6f286099c8607fc639117
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-07-02 02:33:22 +0930

    added background and styling for homepage

--- ap-frontend/src/customerPages/HomePage.jsx
+++ ap-frontend/src/customerPages/HomePage.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-
 function HomePage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-home">
-      <div className="">
-        <h2 className="text-lg font-bold text-center text-white md:text-xl lg:text-2xl">Atelier Pascale</h2>
+    <div className="relative min-h-screen">
+      <div className="bg-home" />
+
+      <div className="absolute inset-0 flex flex-col items-end justify-center pr-10 text-white md:pr-20 lg:pr-30">
+        <h1 className="font-['Mea_Culpa'] text-[3rem] font-thin tracking-[0.15em] pr-15 md:text-[5rem] md:pr-30 lg:text-[7rem] lg:pr-50">
+          Atelier
+        </h1>
+
+        <h1 className="font-['Mea_Culpa'] text-[3rem] font-thin tracking-[0.15em] md:mt-[0.2rem] md:text-[5rem] lg:mt-[0.5rem] lg:text-[7rem]">
+          Pascale
+        </h1>
       </div>
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
