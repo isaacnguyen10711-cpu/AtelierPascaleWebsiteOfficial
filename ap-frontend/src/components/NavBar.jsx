4ee2bc5584d2c95b5c39a05fd14e448352d955a0
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-07-02 02:59:52 +0930

    Updated nav bar

--- ap-frontend/src/components/NavBar.jsx
+++ ap-frontend/src/components/NavBar.jsx
@@ -1,12 +1,17 @@
-import { Link } from 'react-router-dom'
-
+import { Link, useLocation } from 'react-router-dom'
+import LoginPage from '../customerPages/LoginPage';
 function NavBar() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+
+
   return (
     <nav className="fixed top-0 left-0 z-50 w-full bg-transparent border-b border-white shadow-md">
-      <div className="mx-auto flex items-center gap-4 px-4 py-4 font-medium text-white md:gap-6 lg:gap-8">
+      <div className={`mx-auto flex items-center gap-4 px-4 py-4 font-medium text-sm ${isLoginPage ? 'text-black' : 'text-white'} md:gap-6 md:text-md lg:gap-8 lg:text-lg`}>
         <Link to="/">Home</Link>
         <Link to="/products/lacquer">Lacquer</Link>
         <Link to="/products/art">Art</Link>
+        {!isLoginPage && <Link to="/login" className={`flex flex-1 justify-end ${isLoginPage ? 'hover:text-gray-500' : 'hover:text-gray-300'}`}> Login</Link>}
       </div>
     </nav>
   )
