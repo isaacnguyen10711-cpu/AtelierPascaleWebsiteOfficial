77cdccb84467e36151c145580f6363994c503023
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-09 22:35:54 +0930

    Remove Put and Delete action in ordersController

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -4,9 +4,11 @@ using AtelierPascaleWebsite.Models.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 public class OrdersController : ControllerBase
 {
     private readonly DatabaseContext _context;
@@ -16,6 +18,7 @@ public class OrdersController : ControllerBase
     }
 
     // GET: api/Order
+    [Authorize(Roles = "Admin")]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Order>>> GetOrder()
     {
@@ -34,37 +37,6 @@ public class OrdersController : ControllerBase
         }
 
         return order;
-    }
-
-    // PUT: api/Order/5
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-    [HttpPut("{id}")]
-    public async Task<IActionResult> PutOrder(int? id, Order order)
-    {
-        if (id != order.Id)
-        {
-            return BadRequest();
-        }
-
-        _context.Entry(order).State = EntityState.Modified;
-
-        try
-        {
-            await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-            if (!OrderExists(id))
-            {
-                return NotFound();
-            }
-            else
-            {
-                throw;
-            }
-        }
-
-        return NoContent();
     }
 
     // POST: api/Order
@@ -125,27 +97,6 @@ public class OrdersController : ControllerBase
             OrderId = confirmedOrder.Id,
             OrderDate = confirmedOrder.OrderDate
         });
-    }
-
-    // DELETE: api/Order/5
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteOrder(int? id)
-    {
-        var order = await _context.Orders.FindAsync(id);
-        if (order == null)
-        {
-            return NotFound();
-        }
-
-        _context.Orders.Remove(order);
-        await _context.SaveChangesAsync();
-
-        return NoContent();
-    }
-
-    private bool OrderExists(int? id)
-    {
-        return _context.Orders.Any(e => e.Id == id);
     }
 
     private int GetCurrentUserId()
