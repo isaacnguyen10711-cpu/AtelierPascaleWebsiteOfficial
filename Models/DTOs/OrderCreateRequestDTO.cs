53c0239e868a1e1d54a4bc8384af631fc2a7636e
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-09 23:25:40 +0930

    Remove order date from OrderCreateRequest

--- Models/DTOs/OrderCreateRequestDTO.cs
+++ Models/DTOs/OrderCreateRequestDTO.cs
@@ -9,6 +9,5 @@
         public string City { get; set; } = string.Empty;
         public string State { get; set; } = string.Empty;
         public string PostalCode { get; set; } = string.Empty;
-        public DateTime OrderDate { get; set; } = DateTime.Now;
     }
 }
