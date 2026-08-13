a7dec04c5e69b2c2e0ecd6404beba14746fed515
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-13 12:39:51 +0930

    Remove OpenApi package

--- Program.cs
+++ Program.cs
@@ -67,12 +67,6 @@ builder.Services.AddScoped<EmailSender>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 app.UseCors();
 
