1aaa0cfeeb511acb3e1c44ee0ec15c36377c0d43
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-20 13:47:04 +0930

    Added product image validation

--- Models/DTOs/ProductImageDTO.cs
+++ Models/DTOs/ProductImageDTO.cs
@@ -1,9 +1,16 @@
 ﻿namespace AtelierPascaleWebsite.Models.DTOs
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class ProductImageDTO
     {
         public int Id { get; set; }
-        public int ProductId { get; set; } 
+        [Range(1, int.MaxValue)]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Url]
+        [StringLength(2048)]
         public string ImageUrl { get; set; } = string.Empty;
     }
 }
