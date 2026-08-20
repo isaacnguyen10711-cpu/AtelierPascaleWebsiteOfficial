4c0dc847a46604454a77d3556242f006f93df380
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-20 13:47:12 +0930

    Added category validation

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,11 +1,16 @@
 namespace AtelierPascaleWebsite.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class Category
 {
     public int Id { get; set; }
 
+    [Required]
+    [StringLength(100)]
     public string Name { get; set; } = string.Empty;
 
+    [StringLength(1000)]
     public string? Description { get; set; }
 
     public ICollection<Product> Products { get; set; } = new List<Product>();
