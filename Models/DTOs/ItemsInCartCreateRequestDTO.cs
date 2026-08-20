a1337f1e9b291b5ccf5a0d15f3bc41f45b728843
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-20 13:46:37 +0930

    Added cart item creation validation

--- Models/DTOs/ItemsInCartCreateRequestDTO.cs
+++ Models/DTOs/ItemsInCartCreateRequestDTO.cs
@@ -1,7 +1,10 @@
 ﻿namespace AtelierPascaleWebsite.Models.DTOs
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class ItemsInCartCreateRequestDTO
     {
+        [Range(1, int.MaxValue)]
         public int ProductId { get; set; }
     }
 }
