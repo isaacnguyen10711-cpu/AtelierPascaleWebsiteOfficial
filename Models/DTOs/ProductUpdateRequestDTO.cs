4e654f61c9ad174af5be321a865104d7df5b54f2
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-20 13:46:24 +0930

    Added product update validation

--- Models/DTOs/ProductUpdateRequestDTO.cs
+++ Models/DTOs/ProductUpdateRequestDTO.cs
@@ -1,11 +1,24 @@
 ﻿namespace AtelierPascaleWebsite.Models.DTOs
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class ProductUpdateRequestDTO
     {
+        [Range(1, int.MaxValue)]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(2000)]
         public string Description { get; set; } = string.Empty;
+
+        [Range(typeof(decimal), "0.01", "999999.99")]
         public decimal Price { get; set; }
+
+        [Range(1, int.MaxValue)]
         public int CategoryId { get; set; }
         public bool IsNewArrival { get; set; } = false;
     }
