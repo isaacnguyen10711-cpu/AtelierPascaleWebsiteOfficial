8c8f022a04df795087443e6c4c3e3db9da6a6759
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-20 13:43:45 +0930

    Added registration validation

--- Models/DTOs/RegisterRequest.cs
+++ Models/DTOs/RegisterRequest.cs
@@ -1,8 +1,16 @@
 ﻿namespace AtelierPascaleWebsite.Models.DTOs
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class RegisterRequest
     {
+        [Required]
+        [EmailAddress]
+        [StringLength(254)]
         public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 8)]
         public string Password { get; set; } = string.Empty;
 
     }
