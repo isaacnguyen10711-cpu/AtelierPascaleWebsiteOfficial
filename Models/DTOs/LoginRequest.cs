afc0f68755b033b0baa096446c71253c0f2e3255
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-20 13:45:53 +0930

    Added login validation

--- Models/DTOs/LoginRequest.cs
+++ Models/DTOs/LoginRequest.cs
@@ -1,8 +1,16 @@
 ﻿namespace AtelierPascaleWebsite.Models.DTOs
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class LoginRequest
     {
+        [Required]
+        [EmailAddress]
+        [StringLength(254)]
         public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
         public string Password { get; set; } = string.Empty;
     }
 }
