732a6354612e60ee6007960136515d20ae5b6bbf
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-20 13:46:44 +0930

    Added cart quantity validation

--- Models/DTOs/ItemsInCartUpdateRequestDTO.cs
+++ Models/DTOs/ItemsInCartUpdateRequestDTO.cs
@@ -1,7 +1,10 @@
 ﻿namespace AtelierPascaleWebsite.Models.DTOs
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class ItemsInCartUpdateRequestDTO
     {
+        [Range(1, 99)]
         public int Quantity { get; set; }
     }
 }
