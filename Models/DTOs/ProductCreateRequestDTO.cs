2fb3c0fea2d99ad41719379232888ff8822a259e
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-20 13:46:11 +0930

    Added product creation validation

--- Models/DTOs/ProductCreateRequestDTO.cs
+++ Models/DTOs/ProductCreateRequestDTO.cs
@@ -1,10 +1,21 @@
 ﻿namespace AtelierPascaleWebsite.Models.DTOs
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class ProductCreateRequestDTO
     {
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(2000)]
         public string Description { get; set; } = string.Empty;
+
+        [Range(typeof(decimal), "0.01", "999999.99")]
         public decimal Price { get; set; }
+
+        [Range(1, int.MaxValue)]
         public int CategoryId { get; set; }
         public bool IsNewArrival { get; set; } = false;
     }
